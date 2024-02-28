b3ea821dce7613bb08f3f7e9c06d2ea8f791c1af
Emrul Kayes <emrul@blockbonds.io>
2024-02-28 17:10:02 +0600

    add sharedkernel.infrastructure project

---
+++ src/SharedKernel.Infrastructure/ServiceCollectionExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace SharedKernel.Infrastructure;
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection RegisterSharedInfrastructure(this IServiceCollection services)
+    {
+        services.AddTransient<ITimeProvider, TimeProvider>();
+        return services;
+    }
+}
---
+++ src/SharedKernel.Infrastructure/TimeProvider.cs
@@ -0,0 +1,6 @@
+﻿
+namespace SharedKernel.Infrastructure;
+internal sealed class TimeProvider : ITimeProvider
+{
+    public DateTimeOffset TimeNow => DateTimeOffset.UtcNow;
+}
